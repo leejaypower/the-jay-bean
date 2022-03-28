2a16d80a8fb5f884c86170e09f554a7d5fc12390
jay <devejayloper@gmail.com>
2022-03-28 19:52:54 +0900

    ✨ Manage out-of-stock conditions

--- src/js/index.js
+++ src/js/index.js
@@ -34,12 +34,15 @@ function App() {
     const template = this.menu[this.currentCategory]
       .map((item, index) => {
         // index로 해당 메뉴의 고유값을 부여한다.
-        return `<li data-menu-id="${index}">
-    <span class="menu-name">${item.name}</span>
+        return `<li data-menu-id="${index}" >
+    <span class="menu-name ${item.soldOut ? "sold-out" : ""}">${
+          item.name
+        }</span>
     <button
       type="button"
+      class="sold-out-button"
     >
-      품절
+      ${item.soldOutText ? "입고" : "품절"} 
     </button>
     <button
       type="button"
@@ -58,7 +61,6 @@ function App() {
       // menu 배열의 모든 요소를 하나의 문자열로 만들기 위해 join 메서드 사용
       .join("");
 
-    // console.log(menuItemTemplate(espressoMenuName));
     $("#menu-list").innerHTML = template;
   };
 
@@ -76,6 +78,7 @@ function App() {
     // 여기서 객체의 키값은 실시간으로 정해진다.
 
     store.setLocalStorage(this.menu);
+
     paint();
   };
 
@@ -110,7 +113,7 @@ function App() {
   };
 
   // 메뉴를 삭제하는 함수
-  const removeMenuitem = (e) => {
+  const removeMenuItem = (e) => {
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     // 삭제에서 확인을 누르면 메뉴가 삭제된다.
     if (confirm(`"${$menuName.innerText}" 메뉴를 정말 삭제하시겠습니까?`)) {
@@ -123,15 +126,46 @@ function App() {
     }
   };
 
+  // 품절 상태 함수
+  const soldOutMenu = (e) => {
+    const menuId = e.target.closest("li").dataset.menuId;
+    const $soldOutButton = e.target
+      .closest("li")
+      .querySelector(".sold-out-button");
+
+    // console.log($soldOutButton.innerText);
+
+    // soldOut이라는 property를 이런 방법으로 추가할 수도 있다. 자바스크립트의 유연함..
+    this.menu[this.currentCategory][menuId].soldOut =
+      !this.menu[this.currentCategory][menuId].soldOut;
+    // boolean 으로 toggle 기능처럼 만들 수 있다.
+    // 처음에는 undefined라 false -> false인 상태에서 누르면 !가 붙어서 true가 되고 이후 참거짓을 반복한다.
+
+    // 품절버튼 내용 바꾸기
+    this.menu[this.currentCategory][menuId].soldOutText =
+      !this.menu[this.currentCategory][menuId].soldOutText;
+
+    store.setLocalStorage(this.menu);
+    paint();
+  };
+
   $("#menu-list").addEventListener("click", (e) => {
     // 메뉴의 수정 버튼을 눌러 prompt를 통해 이름을 수정한다.
     if (e.target.classList.contains("menu-edit-button")) {
       updateMenuName(e);
+      return;
+      // if문이 여러개이기 때문에 마지막에 return을 붙여 불필요한 연산을 하지 않게 한다.
     }
 
     // 메뉴의 삭제 버튼을 누르면 메뉴 삭제 컨펌(confirm)창이 뜬다.
     if (e.target.classList.contains("menu-remove-button")) {
-      removeMenuitem(e);
+      removeMenuItem(e);
+      return;
+    }
+
+    if (e.target.classList.contains("sold-out-button")) {
+      soldOutMenu(e);
+      return;
     }
   });
 
@@ -156,6 +190,7 @@ function App() {
     countMenu();
   });
 
+  // 카테고리 변경한다.
   $("nav").addEventListener("click", (e) => {
     const isCategoryButton = e.target.classList.contains("menu-category");
     if (isCategoryButton) {
